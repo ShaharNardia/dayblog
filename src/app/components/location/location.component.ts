15545dcf8a758b83862baeb2522b4ac288586fc0
ShaharNardia <Shahar.Nardia@gmail.com>
2019-03-28 05:29:29 +0200

    2

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -2,40 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PlacesService } from 'src/app/shared/location.service';
 
-
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.css']
 })
 export class LocationComponent implements OnInit {
-
-  constructor(
-    private route: ActivatedRoute,
-    private placeSrv: PlacesService
-  ) { }
-  vacationId: string = '';
+  constructor(private route: ActivatedRoute, private placeSrv: PlacesService) {}
+  vacationId  = '';
   places: any[] = [];
   place: any = {};
-  placeId: string = '';
-  isEdit: boolean = false;
+  placeId = '';
+  isEdit = false;
 
   ngOnInit() {
     this.route.params.subscribe(parameters => {
       this.vacationId = parameters.id;
-    })
+    });
   }
 
   submit(place) {
     if (!this.isEdit) {
-      this.placeSrv.insertPlace(place).then(data => {
-
-      });
-    }
-    else {
-      this.placeSrv.updatePlace(this.placeId, place).then(data => {
-
-      });
+      this.placeSrv.insertPlace(place).then(data => {});
+    } else {
+      this.placeSrv.updatePlace(this.placeId, place).then(data => {});
     }
   }
   resetFields() {
@@ -44,15 +34,13 @@ export class LocationComponent implements OnInit {
 
   getVacationPlaces() {
     this.placeSrv.getPlaceByVacationId(this.vacationId).subscribe(data => {
-      let tempArr = [];
+      const tempArr = [];
       data.forEach(doc => {
-        let obj = { id: doc.id, ...doc.data() };
+        const obj = { id: doc.id, ...doc.data() };
         tempArr.push(obj);
-      })
+      });
       this.places.length = 0;
       this.places = tempArr;
     });
-
   }
-
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyCORUxLNm7-esjnFbRFFhwBiZ0CjgfuJKI",
-    authDomain: "dayblog-71dab.firebaseapp.com",
-    databaseURL: "https://dayblog-71dab.firebaseio.com",
-    projectId: "dayblog-71dab",
-    storageBucket: "dayblog-71dab.appspot.com",
-    messagingSenderId: "359885599423"
+  firebaseConfig: {
+    apiKey: 'AIzaSyCORUxLNm7-esjnFbRFFhwBiZ0CjgfuJKI',
+    authDomain: 'dayblog-71dab.firebaseapp.com',
+    databaseURL: 'https://dayblog-71dab.firebaseio.com',
+    projectId: 'dayblog-71dab',
+    storageBucket: 'dayblog-71dab.appspot.com',
+    messagingSenderId: '359885599423'
   }
 };
 
